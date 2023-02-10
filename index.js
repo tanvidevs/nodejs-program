d5e201a89f1f674426806b055ffb27757dcaa985
Tanvi Mirase <112555978+tanvidevs@users.noreply.github.com>
2023-02-10 19:33:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+//commonjs, every file is module (by default)
+//module -encapsulated code (only share minimum)
+const names = require('./4-names');
+console.log(names)
+
+//sayHi('susan')
+//sayHi(john)
+//sayHi(peter)
